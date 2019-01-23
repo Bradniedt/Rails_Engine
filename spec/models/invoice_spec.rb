e6d4c7b993e03c7cbefa6d64522d3a8fe9fd3af6
Brad Niedt <bradleyniedt@gmail.com>
2019-01-22 18:19:42 -0700

    Adds tests for invoice item relationships.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Invoice, type: :model do
     it { should belong_to(:merchant) }
     it { should belong_to(:customer) }
     it { should have_many(:transactions) }
+    it { should have_many(:invoice_items) }
   end
 end
