7f16f67b7edbc7430b78494738f2cf41d331035d
Brad Niedt <bradleyniedt@gmail.com>
2019-01-23 11:51:38 -0700

    Added new test for merchant api.

---
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'returns a json list of merchant data' do
+    one = Merchant.create(name: "Merch1")
+    two = Merchant.create(name: "Merch2")
+    three = Merchant.create(name: "Merch3")
+
+    get '/api/v1/merchants'
+
+    merchants = JSON.parse(response.body)["data"]
+    expect(response).to be_successful
+    expect(merchants.count).to eq(0)
+  end
+end
