4bdfd3aaf61a090225aba35bec9408794d570735
Brad Niedt <bradleyniedt@gmail.com>
2019-01-23 11:43:58 -0700

    Adds changes to customer test.

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe 'Customers API' do
   it 'returns a json list of customer data' do
-    Customer.new(first_name: "Bob", last_name: "Rad")
-    Customer.new(first_name: "Beau", last_name: "Rad")
-    Customer.new(first_name: "Bill", last_name: "Rad")
+    one = Customer.create(first_name: "Bob", last_name: "Rad")
+    two = Customer.create(first_name: "Beau", last_name: "Rad")
+    three = Customer.create(first_name: "Bill", last_name: "Rad")
 
-    get '/api/v1/items'
+    get '/api/v1/customers'
 
-    customers = JSON.parse()
+    customers = JSON.parse(response.body)
     expect(response).to be_successful
-    expect(customers.count).to eq(5)
+    expect(customers.count).to eq(1)
   end
 end
