ae4fb890ab7f7bc23a9f1c10a93a40c608c38eb4
Brad Niedt <bradleyniedt@gmail.com>
2019-01-22 18:14:22 -0700

    Adds model tests for customers and merchants.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Customer, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
+    it { should have_many(:invoices) }
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should have_many(:items) }
+    it { should have_many(:invoices) }
   end
 end
