d731eb1a16a10e81f13b1e5b2ef0783aba853801
Brad Niedt <bradleyniedt@gmail.com>
2019-01-22 20:34:16 -0700

    Adds dependent destroy to all models that have a has many relationship.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,6 @@ class Invoice < ApplicationRecord
   validates_presence_of :customer_id, :merchant_id, :status
   belongs_to :customer
   belongs_to :merchant
-  has_many :transactions
-  has_many :invoice_items
+  has_many :transactions, dependent: :destroy
+  has_many :invoice_items, dependent: :destroy 
 end
