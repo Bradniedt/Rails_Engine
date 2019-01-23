545517abacda10fdd90a970197f2bbdef14ea6d8
Brad Niedt <bradleyniedt@gmail.com>
2019-01-23 16:53:31 -0700

    Adds test for merchant model top revenue method.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:items) }
     it { should have_many(:invoices) }
   end
+  describe 'instance methods' do
+    it '.total_revenue' do
+      merchant = create(:merchant)
+      invoice1 = create(:invoice)
+      invoice_item1 = create(:invoice_item, invoice_id: invoice1.id quantity: 10, unit_price: 10)
+      transaction1 = create(:transaction, invoice_id: invoice1.id)
+      transaction2 = create(:transaction, invoice_id: invoice1.id)
+
+      expect(merchant.top_revenue).to eq(200)
+    end
+  end
 end
