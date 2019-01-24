23c5e2ba31b50dcff02bcc61762d1e8adeb48f78
Brad Niedt <bradleyniedt@gmail.com>
2019-01-24 15:29:33 -0700

    Adds logic to customer find controller to pass test.

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::Customers::FindController < ApplicationController
       render json: CustomerSerializer.new(Customer.find(params[:id]))
     elsif params[:first_name]
       render json: CustomerSerializer.new(Customer.find_by(first_name: params[:first_name]))
+    elsif params[:last_name]
+      render json: CustomerSerializer.new(Customer.find_by(last_name: params[:last_name]))
     end
   end
 end
