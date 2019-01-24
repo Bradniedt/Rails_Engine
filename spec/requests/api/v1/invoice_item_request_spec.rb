5ef50020d444fb9388159214ef535d3be513f8d2
Brad Niedt <bradleyniedt@gmail.com>
2019-01-24 14:15:15 -0700

    Adds invoice item show endpoint test.

--- spec/requests/api/v1/invoice_item_request_spec.rb
+++ spec/requests/api/v1/invoice_item_request_spec.rb
@@ -10,4 +10,14 @@ describe 'InvoiceItems API' do
     expect(response).to be_successful
     expect(invoice_items.count).to eq(3)
   end
+  it 'returns data for a specific invoice_item' do
+    invoice_item1 = create(:invoice_item)
+    id = invoice_item1.id
+
+    get "/api/v1/invoice_items/#{id}"
+
+    invoice_item = JSON.parse(response.body)["data"]
+    expect(response).to be_successful
+    expect(invoice_item["attributes"]["id"]).to eq(invoice_item1.id)
+  end
 end
