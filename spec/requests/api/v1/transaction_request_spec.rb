1ab881a66469b2ad1f972a60fa14c806cbe602f8
Brad Niedt <bradleyniedt@gmail.com>
2019-01-24 14:29:31 -0700

    Adds test for transaction show endpoint.

--- spec/requests/api/v1/transaction_request_spec.rb
+++ spec/requests/api/v1/transaction_request_spec.rb
@@ -10,4 +10,14 @@ describe 'Transactions API' do
     expect(response).to be_successful
     expect(transactions.count).to eq(3)
   end
+  it 'returns data for a specific transaction' do
+    transaction1 = create(:transaction)
+    id = transaction1.id
+
+    get "/api/v1/transactions/#{id}"
+
+    transaction = JSON.parse(response.body)["data"]
+    expect(response).to be_successful
+    expect(transaction["attributes"]["id"]).to eq(transaction1.id)
+  end
 end
