a7e6cf8d414bb119e157501e3f2147c784ad844a
Brad Niedt <bradleyniedt@gmail.com>
2019-01-23 18:47:30 -0700

    Adds code to pass merchant model top revenue test.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,10 +4,7 @@ class Merchant < ApplicationRecord
   has_many :invoices, dependent: :destroy
 
   def top_revenue
-    top = Merchant.select("merchants.*, sum(invoice_items.unit_price) * sum(invoice_items.quantity) AS total_rev").joins(invoices: :invoice_items).joins(invoices: :transactions).where("merchants.id = ? AND transactions.result = ?", self.id, "success").group("merchants.id")
-    binding.pry
+    top = Merchant.select("sum(invoice_items.unit_price * invoice_items.quantity) AS total_rev").joins(invoices: [:invoice_items, :transactions]).where("merchants.id = ?", self.id)[0]
     top.total_rev
-    # sql = 'SELECT sum(invoice_items.unit_price) * sum(invoice_items.quantity) AS total_rev FROM "merchants" INNER JOIN "invoices" ON "invoices"."merchant_id" = "merchants"."id" INNER JOIN "invoice_items" ON "invoice_items"."invoice_id" = "invoices"."id" WHERE (merchants.id = 8) GROUP BY merchants.id'
-    # final = ActiveRecord::Base.connection.execute(sql)
   end
 end
