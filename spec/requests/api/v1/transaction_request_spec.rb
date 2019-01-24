46c47e5056c16f8c643260866dcab0ccb745a529
Brad Niedt <bradleyniedt@gmail.com>
2019-01-24 12:59:36 -0700

    Adds change to test.

--- spec/requests/api/v1/transaction_request_spec.rb
+++ spec/requests/api/v1/transaction_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Transactions API' do
   it 'returns a json list of transaction data' do
-    create_list(:invoice_item, 3)
+    create_list(:transaction, 3)
 
     get '/api/v1/transactions'
 
