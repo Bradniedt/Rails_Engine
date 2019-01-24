5aaeb241817f55edd1d4671cf3fc94e37f7e3030
Brad Niedt <bradleyniedt@gmail.com>
2019-01-24 14:31:36 -0700

    Adds code to transaction routes and controller to pass test for transaction show endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
-      resources :transactions, only: [:index]
+      resources :transactions, only: [:index, :show]
       get '/merchants/:id/revenue', to: "revenue#index"
     end
   end
