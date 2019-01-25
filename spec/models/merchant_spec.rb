064de2b3ccb18a636ee967c53c4bd3543713e64a
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 16:20:51 -0700

    Adds test for model date revenue method.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -17,5 +17,21 @@ RSpec.describe Merchant, type: :model do
 
       expect(merchant1.total_revenue).to eq(200)
     end
+    it '.date_revenue' do
+      merchant1 = create(:merchant)
+      item1 = create(:item)
+      invoice1 = create(:invoice, merchant_id: merchant1.id, created_at: 1.day.ago )
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      transaction1 = create(:transaction, invoice_id: invoice1.id)
+      invoice2 = create(:invoice, merchant_id: merchant1.id, created_at: 1.day.ago)
+      invoice_item2 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      transaction2 = create(:transaction, invoice_id: invoice1.id)
+      invoice3 = create(:invoice, merchant_id: merchant1.id)
+      invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      transaction3 = create(:transaction, invoice_id: invoice1.id)
+      date = 1.day.ago
+
+      expect(merchant1.date_revenue(date)).to eq(200)
+    end
   end
 end
