51525f5a7df00fdec7b8d4e52262214d1fe4612f
Brad Niedt <bradleyniedt@gmail.com>
2019-01-24 20:51:55 -0700

    Adds code to pass item single finder endpoint tests.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { |n| "item#{n}" }
-    description { |n| "a cool item#{n}" }
+    sequence(:name) { |n| "item#{n}" }
+    sequence(:description) { |n| "a cool item #{n}" }
     unit_price { rand(100..1000) }
     merchant
   end
