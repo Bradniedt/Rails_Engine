7673804921889d6e2a2b3ef0cd266b6e0d633fd1
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 17:35:49 -0700

    Adds changes to merchant date revenue test.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -19,19 +19,18 @@ RSpec.describe Merchant, type: :model do
     end
     it '.date_revenue' do
       merchant1 = create(:merchant)
-      item1 = create(:item)
-      invoice1 = create(:invoice, merchant_id: merchant1.id, created_at: 1.day.ago )
+      item1 = create(:item, merchant_id: merchant1.id)
+      invoice1 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-25 09:54:09 UTC" )
       invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 5, unit_price: 5)
       transaction1 = create(:transaction, invoice_id: invoice1.id)
-      invoice2 = create(:invoice, merchant_id: merchant1.id, created_at: 1.day.ago)
-      invoice_item2 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
-      transaction2 = create(:transaction, invoice_id: invoice1.id)
-      invoice3 = create(:invoice, merchant_id: merchant1.id)
-      invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
-      transaction3 = create(:transaction, invoice_id: invoice1.id)
-      date = 1.day.ago
+      invoice2 = create(:invoice, merchant_id: merchant1.id, created_at: "2011-04-21 02:00:00 UTC" )
+      invoice_item2 = create(:invoice_item, item_id: item1.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10, created_at: "2011-04-21 02:00:00 UTC")
+      transaction2 = create(:transaction, invoice_id: invoice2.id, created_at: "2011-04-21 02:00:00 UTC")
 
-      expect(merchant1.date_revenue(date)).to eq(200)
+      date = "2012-03-25 09:54:09 UTC"
+
+      expect(merchant1.date_revenue(date)).to eq(125)
     end
   end
 end
