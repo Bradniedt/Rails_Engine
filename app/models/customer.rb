cb2b95e4030b09da81d86394afcdbe0ad316fe46
Brad Niedt <bradleyniedt@gmail.com>
2019-01-26 14:52:44 -0700

    Adds favorite merchant method to customer model to pass customer merchant test.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,13 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices, dependent: :destroy
+
+  def favorite_merchant
+    Merchant.joins(invoices: [:transactions])
+            .select("merchants.*, sum(invoices.id) AS transaction_count")
+            .where("transactions.result = ? AND invoices.customer_id = ?", "success", self.id)
+            .group("merchants.id")
+            .order("transaction_count DESC")
+            .limit(1)[0]
+  end
 end
