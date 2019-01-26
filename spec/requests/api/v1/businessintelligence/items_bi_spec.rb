329c806c1f2fd4dfc43a6680d4b9ba0dd752e673
Brad Niedt <bradleyniedt@gmail.com>
2019-01-26 13:42:36 -0700

    Adds changes to best day endpoint test.

--- spec/requests/api/v1/businessintelligence/items_bi_spec.rb
+++ spec/requests/api/v1/businessintelligence/items_bi_spec.rb
@@ -58,25 +58,25 @@ describe 'Items BI API' do
   it 'returns the top x items ranked by total quantity sold' do
     merchant1 = create(:merchant)
     item1 = create(:item, merchant_id: merchant1.id)
-    invoice1 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-25 09:54:09 UTC")
-    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
-    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
-    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
-    transaction1 = create(:transaction, invoice_id: invoice1.id)
-    invoice2 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-24 04:54:09 UTC")
+    invoice0 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-24")
+    invoice_item0 = create(:invoice_item, item_id: item1.id, invoice_id: invoice0.id, quantity: 10, unit_price: 10)
+    invoice_item0 = create(:invoice_item, item_id: item1.id, invoice_id: invoice0.id, quantity: 10, unit_price: 10)
+    invoice_item0 = create(:invoice_item, item_id: item1.id, invoice_id: invoice0.id, quantity: 10, unit_price: 10)
+    transaction0 = create(:transaction, invoice_id: invoice0.id)
+    invoice2 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-22")
     invoice_item2 = create(:invoice_item, item_id: item1.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
     invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice2.id, quantity: 2, unit_price: 2)
     transaction2 = create(:transaction, invoice_id: invoice2.id)
-    invoice3 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-23 09:54:09 UTC")
+    invoice3 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-19")
     invoice_item4 = create(:invoice_item, item_id: item1.id, invoice_id: invoice3.id, quantity: 5, unit_price: 5)
     transaction3 = create(:transaction, invoice_id: invoice3.id)
     id = item1.id
 
-    get "/api/v1/items/:id/best_day"
+    get "/api/v1/items/#{id}/best_day"
 
-    best_day = JSON.parse(response.body)["data"]
+    best_day = JSON.parse(response.body)["data"]["attributes"]["created_at"]
 
     expect(response).to be_successful
-    expect(best_day).to eq("2012-03-25 09:54:09 UTC")
+    expect(best_day).to eq("2012-03-24T00:00:00.000Z")
   end
 end
