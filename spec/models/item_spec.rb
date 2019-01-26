8717ae7fe511ddf58adbe9c521a718e8bc82f0ad
Brad Niedt <bradleyniedt@gmail.com>
2019-01-26 12:45:59 -0700

    Adds changes to top items method test to change results.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,23 +11,25 @@ RSpec.describe Item, type: :model do
   end
   describe 'class methods' do
     it '#top_by_revenue(num)' do
-      item1 = create(:item)
-      invoice1 = create(:invoice)
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      item1 = create(:item, merchant_id: merchant1.id)
+      invoice1 = create(:invoice, merchant_id: merchant1.id)
       invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
       invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
       invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
       transaction1 = create(:transaction, invoice_id: invoice1.id)
-      item2 = create(:item)
-      invoice2 = create(:invoice)
+      item2 = create(:item, merchant_id: merchant1.id)
+      invoice2 = create(:invoice, merchant_id: merchant1.id)
       invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
       invoice_item3 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 2, unit_price: 2)
       transaction2 = create(:transaction, invoice_id: invoice2.id)
-      item3 = create(:item)
-      invoice3 = create(:invoice)
+      item3 = create(:item, merchant_id: merchant2.id)
+      invoice3 = create(:invoice, merchant_id: merchant2.id)
       invoice_item4 = create(:invoice_item, item_id: item3.id, invoice_id: invoice3.id, quantity: 5, unit_price: 5)
       transaction3 = create(:transaction, invoice_id: invoice3.id)
 
-      top_items = Items.top_by_revenue(2)
+      top_items = Item.top_by_revenue(2)
 
       expect(top_items[0]["id"]).to eq(item1.id)
       expect(top_items[1]["id"]).to eq(item2.id)
