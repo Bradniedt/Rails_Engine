04eefc70ffbfebcf1c4d37806438507ab567b8d7
Brad Niedt <bradleyniedt@gmail.com>
2019-01-26 13:14:38 -0700

    Adds test for items BI best day endpoint.

--- spec/requests/api/v1/businessintelligence/items_bi_spec.rb
+++ spec/requests/api/v1/businessintelligence/items_bi_spec.rb
@@ -55,4 +55,28 @@ describe 'Items BI API' do
     expect(most_items[0]["id"].to_i).to eq(item1.id)
     expect(most_items[1]["id"].to_i).to eq(item2.id)
   end
+  it 'returns the top x items ranked by total quantity sold' do
+    merchant1 = create(:merchant)
+    item1 = create(:item, merchant_id: merchant1.id)
+    invoice1 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-25 09:54:09 UTC")
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+    transaction1 = create(:transaction, invoice_id: invoice1.id)
+    invoice2 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-24 04:54:09 UTC")
+    invoice_item2 = create(:invoice_item, item_id: item1.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
+    invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice2.id, quantity: 2, unit_price: 2)
+    transaction2 = create(:transaction, invoice_id: invoice2.id)
+    invoice3 = create(:invoice, merchant_id: merchant1.id, created_at: "2012-03-23 09:54:09 UTC")
+    invoice_item4 = create(:invoice_item, item_id: item1.id, invoice_id: invoice3.id, quantity: 5, unit_price: 5)
+    transaction3 = create(:transaction, invoice_id: invoice3.id)
+    id = item1.id
+
+    get "/api/v1/items/best_day?id=#{id}"
+
+    best_day = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(best_day).to eq("2012-03-25 09:54:09 UTC")
+  end
 end
