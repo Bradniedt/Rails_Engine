2f60fd87b19bc1fdac9be3111f0a1cd0810a74c9
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 18:22:11 -0700

    Adds test for merchant favorite customer method.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -32,5 +32,24 @@ RSpec.describe Merchant, type: :model do
 
       expect(merchant1.date_revenue(date)).to eq(125)
     end
+    it 'favorite_customer' do
+      merchant1 = create(:merchant)
+      customer1 = create(:customer)
+      customer2 = create(:customer)
+      customer3 = create(:customer)
+      item1 = create(:item, merchant_id: merchant1.id)
+      invoice1 = create(:invoice, customer_id: customer2.id, merchant_id: merchant1.id)
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 5, unit_price: 5)
+      transaction1 = create(:transaction, invoice_id: invoice1.id)
+      invoice2 = create(:invoice, customer_id: customer2.id, merchant_id: merchant1.id)
+      invoice_item2 = create(:invoice_item, item_id: item1.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
+      transaction2 = create(:transaction, invoice_id: invoice2.id)
+      invoice3 = create(:invoice, customer_id: customer1.id, merchant_id: merchant1.id)
+      invoice_item4 = create(:invoice_item, item_id: item1.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
+      transaction3 = create(:transaction, invoice_id: invoice2.id)
+
+      expect(merchant1.favorite_customer).to eq(customer2)
+    end
   end
 end
