5e39ced62d7b032a7e8dc4618a3678bd06354b8b
Brad Niedt <bradleyniedt@gmail.com>
2019-01-26 12:55:08 -0700

    Adds model test and endpoint test for items most sold.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,5 +34,29 @@ RSpec.describe Item, type: :model do
       expect(top_items[0]["id"]).to eq(item1.id)
       expect(top_items[1]["id"]).to eq(item2.id)
     end
+    it '#top_by_sold' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      item1 = create(:item, merchant_id: merchant1.id)
+      invoice1 = create(:invoice, merchant_id: merchant1.id)
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+      transaction1 = create(:transaction, invoice_id: invoice1.id)
+      item2 = create(:item, merchant_id: merchant1.id)
+      invoice2 = create(:invoice, merchant_id: merchant1.id)
+      invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
+      invoice_item3 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 2, unit_price: 2)
+      transaction2 = create(:transaction, invoice_id: invoice2.id)
+      item3 = create(:item, merchant_id: merchant2.id)
+      invoice3 = create(:invoice, merchant_id: merchant2.id)
+      invoice_item4 = create(:invoice_item, item_id: item3.id, invoice_id: invoice3.id, quantity: 5, unit_price: 5)
+      transaction3 = create(:transaction, invoice_id: invoice3.id)
+
+      most_items = Item.top_by_sold(2)
+      
+      expect(most_items[0]["id"].to_i).to eq(item1.id)
+      expect(most_items[1]["id"].to_i).to eq(item2.id)
+    end
   end
 end
--- spec/requests/api/v1/businessintelligence/items_bi_spec.rb
+++ spec/requests/api/v1/businessintelligence/items_bi_spec.rb
@@ -28,4 +28,31 @@ describe 'Items BI API' do
     expect(top_items[0]["id"].to_i).to eq(item1.id)
     expect(top_items[1]["id"].to_i).to eq(item2.id)
   end
+  it 'returns the top x items ranked by total quantity sold' do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    item1 = create(:item, merchant_id: merchant1.id)
+    invoice1 = create(:invoice, merchant_id: merchant1.id)
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
+    transaction1 = create(:transaction, invoice_id: invoice1.id)
+    item2 = create(:item, merchant_id: merchant1.id)
+    invoice2 = create(:invoice, merchant_id: merchant1.id)
+    invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
+    invoice_item3 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 2, unit_price: 2)
+    transaction2 = create(:transaction, invoice_id: invoice2.id)
+    item3 = create(:item, merchant_id: merchant2.id)
+    invoice3 = create(:invoice, merchant_id: merchant2.id)
+    invoice_item4 = create(:invoice_item, item_id: item3.id, invoice_id: invoice3.id, quantity: 5, unit_price: 5)
+    transaction3 = create(:transaction, invoice_id: invoice3.id)
+
+    get "/api/v1/items/most_items?quantity=2"
+
+    most_items = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(most_items[0]["id"].to_i).to eq(item1.id)
+    expect(most_items[1]["id"].to_i).to eq(item2.id)
+  end
 end
