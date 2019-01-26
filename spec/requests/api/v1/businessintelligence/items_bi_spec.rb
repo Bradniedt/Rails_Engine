f9a8c4010f3fc1d54cdd675d8ef59369f2a622d4
Brad Niedt <bradleyniedt@gmail.com>
2019-01-26 12:51:24 -0700

    Adds changes to test data and expectations for item top by revenue endpoint.

--- spec/requests/api/v1/businessintelligence/items_bi_spec.rb
+++ spec/requests/api/v1/businessintelligence/items_bi_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 describe 'Items BI API' do
   it 'returns the top x items ranked by total revenue generated' do
-    item1 = create(:item)
-    invoice1 = create(:invoice)
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    item1 = create(:item, merchant_id: merchant1.id)
+    invoice1 = create(:invoice, merchant_id: merchant1.id)
     invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
     invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
     invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 10)
     transaction1 = create(:transaction, invoice_id: invoice1.id)
-    item2 = create(:item)
-    invoice2 = create(:invoice)
+    item2 = create(:item, merchant_id: merchant1.id)
+    invoice2 = create(:invoice, merchant_id: merchant1.id)
     invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 10, unit_price: 10)
     invoice_item3 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 2, unit_price: 2)
     transaction2 = create(:transaction, invoice_id: invoice2.id)
-    item3 = create(:item)
-    invoice3 = create(:invoice)
+    item3 = create(:item, merchant_id: merchant2.id)
+    invoice3 = create(:invoice, merchant_id: merchant2.id)
     invoice_item4 = create(:invoice_item, item_id: item3.id, invoice_id: invoice3.id, quantity: 5, unit_price: 5)
     transaction3 = create(:transaction, invoice_id: invoice3.id)
 
@@ -23,7 +25,7 @@ describe 'Items BI API' do
     top_items = JSON.parse(response.body)["data"]
 
     expect(response).to be_successful
-    expect(top_items[0]["id"]).to eq(item1.id)
-    expect(top_items[1]["id"]).to eq(item2.id)
+    expect(top_items[0]["id"].to_i).to eq(item1.id)
+    expect(top_items[1]["id"].to_i).to eq(item2.id)
   end
-end 
+end
