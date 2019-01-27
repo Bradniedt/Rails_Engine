bbf1d0094748fa8310deb45837a5b92677724654
Brad Niedt <bradleyniedt@gmail.com>
2019-01-27 14:56:56 -0700

    Adds small change to invoice items test.

--- spec/requests/api/v1/relationships/invoices_spec.rb
+++ spec/requests/api/v1/relationships/invoices_spec.rb
@@ -53,4 +53,20 @@ describe 'Invoice Relationship Endpoints' do
     expect(items[1]["id"].to_i).to eq(item2.id)
     expect(items[2]["id"].to_i).to eq(item3.id)
   end
+  it 'returns a collection of invoice_items associated with a specific invoice' do
+    invoice = create(:invoice)
+    invoice_item1 = create(:invoice_item, invoice_id: invoice.id)
+    invoice_item2 = create(:invoice_item, invoice_id: invoice.id)
+    invoice_item3 = create(:invoice_item, invoice_id: invoice.id)
+    id = invoice.id
+
+    get "/api/v1/invoices/#{id}/invoice_items"
+
+    invoice_items = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(invoice_items[0]["id"].to_i).to eq(invoice_item1.id)
+    expect(invoice_items[1]["id"].to_i).to eq(invoice_item2.id)
+    expect(invoice_items[2]["id"].to_i).to eq(invoice_item3.id)
+  end
 end
