045577fe14ea76beb77ac6ef7c215753e786ed28
Brad Niedt <bradleyniedt@gmail.com>
2019-01-27 16:46:19 -0700

    Adds changes to test for invoice item relationships endpoints.

--- spec/requests/api/v1/relationships/invoice_items_spec.rb
+++ spec/requests/api/v1/relationships/invoice_items_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe 'Invoice Items Relationship Endpoints' do
   it 'returns the invoice associated with a specific invoice item' do
     merchant1 = create(:merchant)
-    invoice = create(:invoice, merchant: merchant1)
-    invoice_item = create(:invoice_item, invoice_id: invoice.id)
+    invoice1 = create(:invoice, merchant: merchant1)
+    invoice_item = create(:invoice_item, invoice_id: invoice1.id)
     id = invoice_item.id
 
     get "/api/v1/invoice_items/#{id}/invoice"
 
-    invoice = JSON.parse(response.body)["data"]
+    invoice = JSON.parse(response.body)["data"]["attributes"]
 
     expect(response).to be_successful
-    expect(invoice["id"].to_i).to eq(invoice.id)
+    expect(invoice["id"].to_i).to eq(invoice1.id)
   end
 end
