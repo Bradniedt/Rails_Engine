0ed446c273abcd8ff57fb916ab7ed9b3ce802813
Brad Niedt <bradleyniedt@gmail.com>
2019-01-27 13:15:25 -0700

    Adds test for merchant invoices endpoint.

--- spec/requests/api/v1/relationships/merchants_spec.rb
+++ spec/requests/api/v1/relationships/merchants_spec.rb
@@ -11,10 +11,26 @@ describe 'Merchant Relationship Endpoints' do
     get "/api/v1/merchants/#{id}/items"
 
     items = JSON.parse(response.body)["data"]
-    
+
     expect(response).to be_successful
     expect(items[0]["id"].to_i).to eq(item1.id)
     expect(items[1]["id"].to_i).to eq(item2.id)
     expect(items[2]["id"].to_i).to eq(item3.id)
+  end
+  it 'returns a collection of invoices associated with a specific merchant' do
+    merchant = create(:merchant)
+    invoice1 = create(:invoice, merchant_id: merchant.id)
+    invoice2 = create(:invoice, merchant_id: merchant.id)
+    invoice3 = create(:invoice, merchant_id: merchant.id)
+    id = merchant.id
+
+    get "/api/v1/merchants/#{id}/invoices"
+
+    invoices = JSON.parse(response.body)["data"]
+    
+    expect(response).to be_successful
+    expect(invoices[0]["id"].to_i).to eq(invoice1.id)
+    expect(invoices[1]["id"].to_i).to eq(invoice2.id)
+    expect(invoices[2]["id"].to_i).to eq(invoice3.id)
   end
 end
