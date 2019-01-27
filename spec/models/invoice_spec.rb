bce0151451f5f79e5e627dd8c57fceac4dbb379e
Brad Niedt <bradleyniedt@gmail.com>
2019-01-27 13:50:38 -0700

    Adds test for invoice items method.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -11,19 +11,21 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:invoice_items) }
   end
   describe 'instance methods' do
-    merchant = create(:merchant)
-    invoice = create(:invoice, merchant_id: merchant.id)
-    item1 = create(:item, merchant_id: merchant.id, )
-    item2 = create(:item, merchant_id: merchant.id, )
-    item3 = create(:item, merchant_id: merchant.id, )
-    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice.id)
-    invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice.id)
-    invoice_item3 = create(:invoice_item, item_id: item3.id, invoice_id: invoice.id)
+    it '.items' do
+      merchant = create(:merchant)
+      invoice = create(:invoice, merchant_id: merchant.id)
+      item1 = create(:item, merchant_id: merchant.id, )
+      item2 = create(:item, merchant_id: merchant.id, )
+      item3 = create(:item, merchant_id: merchant.id, )
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice.id)
+      invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice.id)
+      invoice_item3 = create(:invoice_item, item_id: item3.id, invoice_id: invoice.id)
 
-    items = Invoice.items
+      items = invoice.items
 
-    expect(items[0]["id"].to_i).to eq(item1.id)
-    expect(items[1]["id"].to_i).to eq(item2.id)
-    expect(items[2]["id"].to_i).to eq(item3.id)
+      expect(items[0]["id"].to_i).to eq(item1.id)
+      expect(items[1]["id"].to_i).to eq(item2.id)
+      expect(items[2]["id"].to_i).to eq(item3.id)
+    end
   end
 end
