8ce34d998ab46d89bdc8f1652ffa1f8f3d937000
Brad Niedt <bradleyniedt@gmail.com>
2019-01-27 17:14:17 -0700

    Adds new test for customer transactions relationship endpoint.

--- spec/requests/api/v1/relationships/customers_spec.rb
+++ spec/requests/api/v1/relationships/customers_spec.rb
@@ -15,4 +15,20 @@ describe 'Customer Relationship Endpoints' do
     expect(invoices[0]["id"].to_i).to eq(invoice1.id)
     expect(invoices[1]["id"].to_i).to eq(invoice2.id)
   end
+  it 'returns a collection of transactions associated with a specific customer' do
+    customer1 = create(:customer)
+    invoice1 = create(:invoice, customer_id: customer1.id)
+    invoice2 = create(:invoice, customer_id: customer1.id)
+    transaction1 = create(:transaction, invoice_id: invoice_1.id)
+    transaction2 = create(:transaction, invoice_id: invoice_2.id)
+    id = customer1.id
+
+    get "/api/v1/customers/#{id}/transactions"
+
+    transactions = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(transactions[0]["id"].to_i).to eq(transaction1.id)
+    expect(transactions[1]["id"].to_i).to eq(transaction2.id)
+  end
 end
